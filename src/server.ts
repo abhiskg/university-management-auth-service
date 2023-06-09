f62f57f9f0c8ed323e9f3522306e42f0b05e7062
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-06-09 11:55:04 +0600

    Server updated

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
-import app from './app'
-import config from './config'
-import dbConnect from './config/dbConnect'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
-dbConnect()
-  .then(() => {
+(async function dbConnect() {
+  try {
+    await mongoose.connect(config.mongo_uri, {
+      dbName: 'university-management',
+    });
+    console.log('Database connected Successfully');
+
     app.listen(config.port, () => {
-      console.log(`Database connected and listing on port ${config.port}`)
-    })
-  })
-  .catch((err) => {
-    console.log((err as Error).message)
-  })
+      console.log(`Application listing on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log('Failed to connect');
+  }
+})();
+
+// dbConnect()
+//   .then(() => {
+//     app.listen(config.port, () => {
+//       console.log(`Database connected and listing on port ${config.port}`)
+//     })
+//   })
+//   .catch((err) => {
+//     console.log((err as Error).message)
+//   })
