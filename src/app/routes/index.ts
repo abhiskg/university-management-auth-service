72324e3ff51de72936bcf2798b2e5403373a51a7
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-06-14 13:35:46 +0600

    Academic Faculty done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { UserRoutes } from "../modules/user/user.route";
 import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.route";
+import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
 
 const router = express.Router();
 
 router.use("/users", UserRoutes);
 router.use("/academic-semesters", AcademicSemesterRoutes);
+router.use("/academic-faculties", AcademicFacultyRoutes);
 
 export const RootRoute = router;
