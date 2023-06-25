d1616de02b1efca8b0b2c28afdfd009fe6969a5d
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-06-25 22:57:20 +0600

    Jwt helper fn added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 import path from "path";
+import type { Secret } from "jsonwebtoken";
 
 // cwd means current directory
 dotenv.config({ path: path.join(process.cwd(), ".env") });
@@ -12,4 +13,11 @@ export default {
   default_student_password: process.env.DEFAULT_STUDENT_PASSWORD as string,
   default_faculty_password: process.env.DEFAULT_FACULTY_PASSWORD as string,
   default_admin_password: process.env.DEFAULT_ADMIN_PASSWORD as string,
+
+  jwt: {
+    access_secret: process.env.JWT_ACCESS_SECRET as Secret,
+    refresh_secret: process.env.JWT_REFRESH_SECRET as Secret,
+    access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN as string,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as string,
+  },
 };
